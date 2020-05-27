9dc254d03385db7fc8ae74df3124734f23fe8ca4
the-w3b-dev <65990737+the-w3b-dev@users.noreply.github.com>
2020-05-27 07:44:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 
 const audio = document.getElementById('audio');
-const progress =document.getElementById('progress');
-const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title')
 const cover = document.getElementById('cover')
 
@@ -68,10 +66,6 @@ function nextSong() {
 
     playSong();
 }
-
-// Update progress bar
-
-// Set progress bar
 
 // Event listeners
 playBtn.addEventListener('click', () => {
